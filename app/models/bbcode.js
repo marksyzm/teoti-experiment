5e06d2ac0a93b57d7dce3d5651947771b7f55ac7
Mark Elphinstone-Hoadley <mark@midair.info>
2014-10-21 22:49:36 +0100

    Added BBCode schema

---
+++ app/models/bbcode.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var mongoose = require("mongoose"),
+    Schema = mongoose.Schema,
+    schemaName = "Bbcode";
+
+var BbcodeSchema = new Schema({
+    title           : { type: String, required: true },
+    description     : { type: String },
+    code            : { type: String, required: true },
+    display         : { type: String, enum: [ "inline", "block" ], default: "inline" },
+    callback        : { type: String }
+});
+
+mongoose.model(schemaName, BbcodeSchema);
+
+module.exports = mongoose;

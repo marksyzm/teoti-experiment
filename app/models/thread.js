43c34726d0e47bcb04fe269c5728df6c637b9596
Mark Elphinstone-Hoadley <mark@midair.info>
2014-10-21 22:12:58 +0100

    Added more thread schema properties

--- app/models/thread.js
+++ app/models/thread.js
@@ -13,9 +13,19 @@ var ThreadSchema = new Schema({
     firstPost       : { type: Number, ref: "Post" },
     firstPostUser   : { type: Number, ref: "User" },
     lastPost        : { type: Number, ref: "Post" },
+    lastPostUser    : { type: Number, ref: "User" },
     forum           : { type: Number, ref: "Forum" },
-    open            : { type: Boolean, required: true, default: true },
-    replyCount      : { type: Number, default: 0 }
+    created         : { type: Date, default: Date.now },
+    replyCount      : { type: Number, default: 0 },
+    viewCount       : { type: Number, default: 0 },
+    open            : { type: Boolean, default: true },
+    deleted         : { type: Boolean, default: false },
+    sticky          : { type: Boolean, default: false },
+    description     : { type: String, trim: true },
+    related         : { type: String },
+    thumbnail       : { type: String },
+    score           : { type: Number, default: 0 },
+    style           : { type: Number, ref: "Style" }
 });
 
 ThreadSchema.plugin(autoIncrement.plugin, schemaName);

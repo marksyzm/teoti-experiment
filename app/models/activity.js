09e0943ca5d515157ddffdf1fb43fe031b8a70f6
Mark Elphinstone-Hoadley <mark@midair.info>
2014-11-08 00:09:04 +0000

    Added activity schema

---
+++ app/models/activity.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var _ = require("lodash"),
+    mongoose = require("../data/db").mongoose,
+    notificationTypes = require("../data/defaults/notification-types.json"),
+    Schema = mongoose.Schema,
+    schemaName = "Activity",
+    notificationTypeIds = _.pluck(notificationTypes, "id");
+
+var ActivitySchema = new Schema({
+    user:           { type: Number, ref: "User", required: true },
+    itemid:         { type: Number, required: true },
+    forum:          { type: Number, ref: "Forum", required: true },
+    method:         { type: String, enum: [ "create", "update", "delete" ], required: true, default: "create" },
+    type:           { type: Number, enum: notificationTypeIds, required: true },
+    created:        { type: Date, default: Date.now, required: true },
+    value:          { type: String },
+    ip:             { type: String },
+    browser:        { type: String },
+    browserVersion: { type: String },
+    os:             { type: String },
+    osVersion:      { type: String },
+    sessionId:      { type: String },
+    script:         { type: String },
+    line:           { type: Number }
+});
+
+mongoose.model(schemaName, ActivitySchema);
+
+module.exports = mongoose;

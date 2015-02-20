7936dc3bb49149f1811692e1fea2cd99309ba73d
Mark Elphinstone-Hoadley <mark@midair.info>
2015-02-20 23:35:47 +0000

    Tidied up index stuff into middleware script

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,13 +6,13 @@ module.exports = function (grunt) {
         configDir: require("path").resolve("tasks")
     });
 
-    grunt.registerTask("test-cov",    [ "env:test", "mongoimport", "lint", "mochacli:tests", "mocha_istanbul:coverage"]);
-    grunt.registerTask("test",        [ "env:test", "mongoimport", "lint", "mochacli:tests" ]);
-    grunt.registerTask("coverage",    [ "env:test", "mongoimport", "lint", "mocha_istanbul:coverage" ]);
-    grunt.registerTask("coveralls",   [ "env:test", "mongoimport", "lint", "mocha_istanbul:coveralls" ]);
+    grunt.registerTask("test-cov",    [ "env:test", "browserify:dist", "lint", "mochacli:tests", "mocha_istanbul:coverage"]);
+    grunt.registerTask("test",        [ "env:test", "browserify:dist", "lint", "mochacli:tests" ]);
+    grunt.registerTask("coverage",    [ "env:test", "browserify:dist", "lint", "mocha_istanbul:coverage" ]);
+    grunt.registerTask("coveralls",   [ "env:test", "browserify:dist", "lint", "mocha_istanbul:coveralls" ]);
     grunt.registerTask("dev",         [ "build", "concurrent:target" ]);
-    grunt.registerTask("build",       [ "browserify:dev", "lint", "sass:dev" ]);
-    grunt.registerTask("default",     [ "browserify:dist", "lint", "sass:dist" ]);
+    grunt.registerTask("build",       [ "lint", "browserify:dev", "sass:dev" ]);
+    grunt.registerTask("default",     [ "lint", "browserify:dist", "sass:dist" ]);
     grunt.registerTask("package",     [ "default"]);
     grunt.registerTask("lint",        [ "jshint" ]);
 };

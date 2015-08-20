b065d95e08a05465fc30b35158458008cef244c2
Mark Elphinstone-Hoadley <mark@oxfordsourceltd.com>
2015-08-20 13:04:09 +0100

    Added pagination to thread and user components. Implemented ui-router.

--- build/tasks/nodemon.js
+++ build/tasks/nodemon.js
@@ -6,12 +6,13 @@ var nodemon = require('gulp-nodemon');
 gulp.task('nodemon', function () {
     return nodemon({
         ignore: [
+            'lib/browser/**',
             'node_modules/**',
             'public/js/**',
             'tasks/**'
         ],
         script: 'nodemon-combined.js',
-        ext: 'js jsx',
+        ext: 'js',
         env: { 'NODE_ENV': 'development' }
     });
 })

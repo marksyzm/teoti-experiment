5c9a1e0e2c1eb7fd2ee476f71fb6292a7cbcdae9
Mark Elphinstone-Hoadley <mark@oxfordsourceltd.com>
2015-08-30 00:01:09 +0100

    Added 2 column layout and autoprefixer to simple layout

--- build/tasks/sass.js
+++ build/tasks/sass.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourceMaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     return gulp.src('./public/sass/style.scss')
         .pipe(sourceMaps.init())
         .pipe(sass())
+        .pipe(autoprefixer('last 2 version'))
         .pipe(sourceMaps.write('./'))
         .pipe(gulp.dest('./public/css/'));
 });
